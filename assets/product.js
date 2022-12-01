23dbcff9967023f4e14d55e9a054fd028b23c7d6
ybenadda <117163420+ybenadda@users.noreply.github.com>
2022-12-01 11:36:08 +0100

    DOT-6873: add upload image

--- assets/product.js
+++ assets/product.js
@@ -2,3 +2,9 @@ function previewProductImage(url) {
   const thumbnail = document.querySelector('#thumbnail')
   thumbnail.src = url
 }
+
+
+function uploadImage() {
+  const uploadInput = document.querySelector('#yc-upload')
+  uploadInput.click()
+}
