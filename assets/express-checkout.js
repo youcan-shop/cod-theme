4d8b6889b207d019daa53caedb8f5b3f48b5759f
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-02 10:32:47 +0100

    update the script

---
+++ assets/express-checkout.js
@@ -0,0 +1,35 @@
+document.getElementById('express-checkout-form').addEventListener('submit', async function (e) {
+  e.preventDefault()
+
+  const fields = Object.fromEntries(new FormData(e.target))
+
+  load('#loading__checkout')
+  try {
+    const productVariantId = document.getElementById('variantId').value
+    const quantity = document.getElementById('quantity').value
+    const response = await youcanjs.checkout.placeExpressCheckoutOrder({ productVariantId, quantity, fields })
+
+    response
+      .onSuccess((data, redirectToThankyouPage) => {
+        redirectToThankyouPage()
+      })
+      .onValidationErr((data) => {
+        notify(data, 'error')
+        console.warn('validation error', data)
+      })
+      .onSkipShippingStep((data, redirectToShippingPage) => {
+        redirectToShippingPage()
+      })
+      .onSkipPaymentStep((data, redirectToPaymentPage) => {
+        redirectToPaymentPage()
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  } catch (e) {
+    console.error(e)
+    notify(e.message, 'error')
+  } finally {
+    stopLoad('#loading__checkout')
+  }
+})
