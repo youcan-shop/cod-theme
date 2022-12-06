e634f8f00994d95ea0a82e5dc565898268423845
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-06 14:40:25 +0100

    update base style and script

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,27 +1,36 @@
-const x = setInterval(() => {
-  let countDownDate = +localStorage.getItem('countDownDate')
+const now = new Date();
+const storedDate = localStorage.getItem('targetDate');
+let targetDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0, 0);
 
-  if (!countDownDate || countDownDate < new Date().getTime()) {
-    countDownDate = new Date().getTime() + 9 * 60 * 60 * 1000
-    localStorage.setItem('countDownDate', countDownDate)
-  }
+if (storedDate) {
+  targetDate = new Date(storedDate);
+} else {
+  localStorage.setItem('targetDate', targetDate);
+}
 
-  const now = new Date().getTime()
-  const distance = countDownDate - now
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+function initCountdown() {
+  const currentDate = new Date();
+  let timeRemaining = targetDate - currentDate;
+  
+  setInterval(() => {
+    timeRemaining = targetDate - new Date();
+    
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    
+    document.getElementById('days').innerHTML = days + '<span class="unit">Days</span>';
+    document.getElementById('hours').innerHTML = hours + '<span class="unit">Hours</span>';
+    document.getElementById('minutes').innerHTML = minutes + '<span class="unit">Minutes</span>';
+    document.getElementById('seconds').innerHTML = seconds + '<span class="unit">Seconds</span>';
+  }, 1000);
+  
+  setInterval(() => {
+    const progress = (timeRemaining / (targetDate - currentDate) * 100);
+    
+    document.getElementById('progress').style.width = progress + '%';
+  }, 1000);
+}
 
-  // Display the result in the element with id="demo"
-  document.getElementById('days').innerHTML = days + '<div class="unit-title">DAYS</div>'
-  document.getElementById('hours').innerHTML = hours + '<div class="unit-title">HOURS</div>'
-  document.getElementById('minutes').innerHTML = minutes + '<div class="unit-title">MINUTES</div>'
-  document.getElementById('seconds').innerHTML = seconds + '<div class="unit-title">SECONDS</div>'
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x)
-    document.getElementById('demo').innerHTML = 'EXPIRED'
-  }
-}, 1000)
+initCountdown();
