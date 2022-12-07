f456057d977a42068cb598e7ad24cc1a9513eb49
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-07 15:24:41 +0100

    update script

--- assets/stock.js
+++ assets/stock.js
@@ -1,4 +1,13 @@
-const progressBarFillWidth = (+stockLeft / +totalInStock) * 100
+let progressBarFillWidth = 0
 
-document.getElementById('current-stock').innerHTML = +stockLeft
+if (typeof stockLeft !== 'number' || typeof totalInStock !== 'number') {
+  throw new Error('stockLeft and totalInStock must be numbers')
+}
+
+if (stockLeft > 0 && totalInStock > 0) {
+  progressBarFillWidth = (stockLeft / totalInStock) * 100
+  console.log('stockLeft: ', progressBarFillWidth)
+}
+
+document.getElementById('current-stock').innerHTML = stockLeft
 document.getElementById('stock-progress').style.width = progressBarFillWidth + '%'
