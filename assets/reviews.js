74d2880d182928845fd69c63f226621319914ccc
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-07 11:32:41 +0100

    Sections > Product reviews (#30)
    * add base reviews's styles and scripts
    
    * Separate script in js file, update style
    
    * add line break

---
+++ assets/reviews.js
@@ -0,0 +1,35 @@
+
+(async () => {
+  const reviewsWrapper = document.querySelector('.yc-reviews-wrapper')
+  const noDataSetter = (element) => {
+    reviewsWrapper.innerHTML = `
+        <div class='review-item__empty'>No reviews available</div>
+      `
+  }
+
+  try {
+    const reviews = await youcanjs.product.fetchReviews(productId).data()
+
+    reviews.forEach((review) => {
+      const reviewItem = document.createElement('li')
+      reviewItem.classList.add('review-item')
+      reviewItem.innerHTML = `
+        <div class='header'>
+          <img class='image' src=${review.images_urls[0] || defaultAvatar} />
+          <div class='name'>${review.first_name} ${review.last_name}</div>
+        </div>
+        <div class='content'>
+          ${review.content}
+        </div>
+      `
+      reviewsWrapper.appendChild(reviewItem)
+    })
+
+    if (reviews.length === 0) {
+      noDataSetter()
+    }
+  } catch (error) {
+    noDataSetter()
+    console.error(error)
+  }
+})()
