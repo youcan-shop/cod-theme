33fc853980a60ff727cd8cc2012e74c306c36dd3
Youssef <117163420+ybenadda@users.noreply.github.com>
2022-12-08 11:05:32 +0100

    Add Countdown and Stock as blocks in Product Section (#38)

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,26 +1,25 @@
 function initCountdown() {
   const currentDate = new Date();
   let timeRemaining = targetDate - currentDate;
-  
-  setInterval(() => {
+
+  const countdown = () => {
     timeRemaining = targetDate - new Date();
     
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    const progress = (timeRemaining / (targetDate - currentDate) * 100);
     
     document.getElementById('days').innerHTML = days + '<span class="unit">Days</span>';
     document.getElementById('hours').innerHTML = hours + '<span class="unit">Hours</span>';
     document.getElementById('minutes').innerHTML = minutes + '<span class="unit">Minutes</span>';
     document.getElementById('seconds').innerHTML = seconds + '<span class="unit">Seconds</span>';
-  }, 1000);
-  
-  setInterval(() => {
-    const progress = (timeRemaining / (targetDate - currentDate) * 100);
-    
     document.getElementById('progress').style.width = progress + '%';
-  }, 1000);
+  }
+  
+  countdown()
+  setInterval(countdown, 1000);
 }
 
 initCountdown();
