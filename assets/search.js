edf4bc5ada305033ca2e5f59730a13442224d1e9
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-08 08:27:01 +0100

    add basic styles and scripts

---
+++ assets/search.js
@@ -0,0 +1,26 @@
+const paginateBtnPrev = document.querySelector('.paginate-btn-prev')
+const paginateBtnNext = document.querySelector('.paginate-btn-next')
+const paginateBtnCurrent = document.querySelectorAll('.paginate-btn')
+
+const urlParams = new URLSearchParams(window.location.search)
+const query = urlParams.get('q')
+let page = +urlParams.get('page[some_unique_id]')
+
+if (page <= 1 || !page) {
+  page = 1
+  paginateBtnPrev.remove()
+}
+
+if (paginateBtnPrev) {
+  paginateBtnPrev.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${page - 1}`)
+}
+
+if (paginateBtnNext) {
+  paginateBtnNext.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${page + 1}`)
+}
+
+if (paginateBtnCurrent) {
+  paginateBtnCurrent.forEach((btn) => {
+    btn.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${btn.dataset.index}`)
+  })
+}
