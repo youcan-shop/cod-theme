fa58d11f5d96f33499111571dd0fb5bfc2d258e2
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-08 09:31:56 +0100

    Snippets > Stock (#32)
    ## QA Steps
    
    -  [ ] `pnpm i`
    -  [ ] `pnpm dev`

---
+++ assets/stock.js
@@ -0,0 +1,12 @@
+let progressBarFillWidth = 0
+
+if (typeof stockLeft !== 'number' || typeof totalInStock !== 'number') {
+  throw new Error('stockLeft and totalInStock must be numbers')
+}
+
+if (stockLeft > 0 && totalInStock > 0) {
+  progressBarFillWidth = (stockLeft / totalInStock) * 100
+}
+
+document.getElementById('current-stock').innerHTML = stockLeft
+document.getElementById('stock-progress').style.width = progressBarFillWidth + '%'
