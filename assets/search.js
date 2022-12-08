e28834c86018abb6d63997d9e2505e4b7d53c5d4
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-08 16:37:29 +0100

    add sorting duntionality

--- assets/search.js
+++ assets/search.js
@@ -1,10 +1,34 @@
 const paginateBtnPrev = document.querySelector('.paginate-btn-prev')
 const paginateBtnNext = document.querySelector('.paginate-btn-next')
 const paginateBtnCurrent = document.querySelectorAll('.paginate-btn')
-
+const searchTitle = document.querySelector('#search-title')
 const urlParams = new URLSearchParams(window.location.search)
 const query = urlParams.get('q')
-let page = +urlParams.get('page[some_unique_id]')
+const sortField = urlParams.get('sort_field')
+const sortOrder = urlParams.get('sort_order')
+let page = +urlParams.get('page[cod]')
+
+const convertUrl = (key, value) => {
+  const url = new URL(window.location.href)
+  if (url.searchParams.has(key)) {
+    url.searchParams.set(key, value)
+  } else {
+    url.searchParams.append(key, value)
+  }
+  return url
+}
+
+const convertUrlWithMultipleQuery = (keys, values) => {
+  const url = new URL(window.location.href)
+  keys.forEach((key, i) => {
+    if (url.searchParams.has(key)) {
+      url.searchParams.set(key, values[i])
+    } else {
+      url.searchParams.append(key, values[i])
+    }
+  })
+  return url
+}
 
 if (page <= 1 || !page) {
   page = 1
@@ -15,15 +39,41 @@ if (page <= 1 || !page) {
 }
 
 if (paginateBtnPrev) {
-  paginateBtnPrev.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${page - 1}`)
+  paginateBtnPrev.setAttribute('href', convertUrl('page[cod]', page - 1))
 }
 
 if (paginateBtnNext) {
-  paginateBtnNext.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${page + 1}`)
+  paginateBtnNext.setAttribute('href', convertUrl('page[cod]', page + 1))
 }
 
 if (paginateBtnCurrent) {
-  paginateBtnCurrent.forEach((btn) => {
-    btn.setAttribute('href', `/search?q=${query}&page[some_unique_id]=${btn.dataset.index}`)
+  paginateBtnCurrent.forEach((btn) => btn.setAttribute('href', convertUrl('page[cod]', btn.dataset.index)))
+}
+
+if (searchTitle) {
+  searchTitle.innerHTML = query
+}
+
+const sortBtns = document.querySelectorAll('.sort-items-holder .pawn')
+
+if (sortBtns) {
+  sortBtns.forEach((btn) => {
+    const convertedSortOrder = sortOrder === 'asc' ? 'desc' : 'asc'
+    btn.setAttribute('href', convertUrlWithMultipleQuery(['sort_field', 'sort_order'], [btn.dataset.value, convertedSortOrder]))
+
+    const sortBtn = document.querySelector(`.sort-items-holder .pawn[data-value='${sortField}']`)
+
+    sortBtn.classList.add('active')
+
+    if (sortBtn) {
+      const sortBtnIcon = sortBtn.querySelector('ion-icon')
+      if (sortBtnIcon) {
+        if (sortOrder === 'asc') {
+          sortBtnIcon.setAttribute('name', 'chevron-up-outline')
+        } else {
+          sortBtnIcon.setAttribute('name', 'chevron-down-outline')
+        }
+      }
+    }
   })
 }
