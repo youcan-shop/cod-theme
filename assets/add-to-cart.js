c1a4cde4576c9d8646b733a73724685503bed6d1
ybenadda <117163420+ybenadda@users.noreply.github.com>
2022-12-09 11:56:34 +0100

    fix style conflic

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,7 +1,7 @@
 async function addToCart() {
   const variantId = document.getElementById('variantId')?.value
   const quantity = document.getElementById('quantity')?.value
-  const uploadedImageLink = document.querySelector('#yc-upload-link');
+  const uploadedImageLink = document.querySelector('#yc-upload-link')?.value;
 
   if (!variantId)
     return notify('Please select a variant', 'error')
@@ -14,7 +14,7 @@ async function addToCart() {
     const response = await youcanjs.cart.addItem({ productVariantId: variantId, attachedImage: uploadedImageLink, quantity })
     if(response.error) throw new Error(response.error)
     stopLoad('#loading__checkout')
-    notify(response.message, 'success')
+    notify("Item has been added successfully", 'success')
   }
   catch (err) {
     stopLoad('#loading__checkout')
