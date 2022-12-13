ee82a45b1a4bac978c732f20bbfca377dca9a515
ybenadda <117163420+ybenadda@users.noreply.github.com>
2022-12-13 15:04:35 +0100

    update theme

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,7 +1,7 @@
 async function addToCart() {
-  const variantId = document.getElementById('variantId')?.value;
+  const variantId = document.getElementById('variantId')?.value || undefined;
   const quantity = document.getElementById('quantity')?.value || 1;
-  const uploadedImageLink = document.querySelector('#yc-upload-link')?.value;
+  const uploadedImageLink = document.querySelector('#yc-upload-link')?.value || undefined;
 
   try {
     load('#loading__checkout');
