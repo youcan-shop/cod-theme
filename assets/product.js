3442b08f995935afa65b8629ed9ec89387a91325
Youssef <117163420+ybenadda@users.noreply.github.com>
2022-12-14 16:07:27 +0100

    Fix > product with no variants error (#86)
    ## JIRA Ticket
    
    [DOT-X](https://nextmediama.atlassian.net/browse/DOT-X)
    
    ## QA Steps
    
    -  [ ] `pnpm i`
    -  [ ] `pnpm dev`
    
    ## Note
    
    Leave empty when you have nothing to say

--- assets/product.js
+++ assets/product.js
@@ -113,6 +113,11 @@ function selectDefaultOptions() {
   });
 
   const selectedVariant = getSelectedVariant();
+
+  if (!selectedVariant) {
+    setDefaultVariant(variants[0]?.id);
+  }
+
   setDefaultVariant(selectedVariant.id);
 }
 
@@ -200,4 +205,3 @@ if (productDetails) {
 }
 
 selectDefaultOptions();
-setDefaultVariant(variants[0].id);
