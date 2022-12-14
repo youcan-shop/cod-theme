2582ecd10e551fd416d81bbc7f92ba54ac53bb04
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-14 14:03:08 +0100

    Fix > Address any issues with the stock/countdown section (#81)
    ## QA Steps
    
    -  [ ] `pnpm i`
    -  [ ] `pnpm dev`

--- assets/countdown.js
+++ assets/countdown.js
@@ -10,12 +10,23 @@ function initCountdown() {
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
     const progress = (timeRemaining / (targetDate - currentDate)) * 100;
+    const units = { days, hours, minutes, seconds };
 
-    document.getElementById('days').innerHTML = days + '<span class="unit">Days</span>';
-    document.getElementById('hours').innerHTML = hours + '<span class="unit">Hours</span>';
-    document.getElementById('minutes').innerHTML = minutes + '<span class="unit">Minutes</span>';
-    document.getElementById('seconds').innerHTML = seconds + '<span class="unit">Seconds</span>';
-    document.getElementById('progress').style.width = progress + '%';
+    for (const unit in units) {
+      const el = document.getElementById(unit);
+      if (units[unit] === 0) {
+        el.style.display = 'none';
+      } else if (el) {
+        el.innerHTML = units[unit] + '<span class="unit">' + unit + '</span>';
+      }
+    }
+
+    const progressEl = document.getElementById('progress');
+    progressEl.style.width = progress + '%';
+
+    if (timeRemaining < 0) {
+      document.querySelector('.countdown-wrapper').innerHTML = 'Offer Is Expired';
+    }
   };
 
   countdown();
