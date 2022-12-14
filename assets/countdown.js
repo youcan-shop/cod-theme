f3411045724bc2f82a601209dd6f80f2777d803e
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2022-12-14 14:00:46 +0100

    remove try catch

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,32 +3,30 @@ function initCountdown() {
   let timeRemaining = targetDate - currentDate;
 
   const countdown = () => {
-    try {
-      timeRemaining = targetDate - new Date();
+    timeRemaining = targetDate - new Date();
 
-      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-      const progress = (timeRemaining / (targetDate - currentDate)) * 100;
-      const units = { days, hours, minutes, seconds };
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    const progress = (timeRemaining / (targetDate - currentDate)) * 100;
+    const units = { days, hours, minutes, seconds };
 
-      for (const unit in units) {
-        const el = document.getElementById(unit);
-        if (units[unit] === 0) {
-          el.style.display = 'none';
-        } else if (el) {
-          el.innerHTML = units[unit] + '<span class="unit">' + unit + '</span>';
-        }
+    for (const unit in units) {
+      const el = document.getElementById(unit);
+      if (units[unit] === 0) {
+        el.style.display = 'none';
+      } else if (el) {
+        el.innerHTML = units[unit] + '<span class="unit">' + unit + '</span>';
       }
+    }
 
-      const progressEl = document.getElementById('progress');
-      progressEl.style.width = progress + '%';
+    const progressEl = document.getElementById('progress');
+    progressEl.style.width = progress + '%';
 
-      if (timeRemaining < 0) {
-        document.querySelector('.countdown-wrapper').innerHTML = 'Offer Is Expired';
-      }
-    } catch (error) {}
+    if (timeRemaining < 0) {
+      document.querySelector('.countdown-wrapper').innerHTML = 'Offer Is Expired';
+    }
   };
 
   countdown();
