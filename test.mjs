f7340434e7feb97851320afab5fd7bb411ea67ee
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2023-01-05 10:55:27 +0100

    remove unneeded comments

--- test.mjs
+++ test.mjs
@@ -1,16 +1,14 @@
 import watch from 'node-watch';
 import { sync } from './helpers/sync.mjs';
-import { exec } from 'child_process';
 import prettier from 'prettier';
 
 const watchFiles = async () => {
   watch('./', { recursive: true }, (evt, name) => {
-    // const command = `prettier --write --ignore-unknown ./${name} --plugin-search-dir=. --ignore-path=.prettierignore`;
     prettier.resolveConfig('./.prettierrc').then((options) => {
       prettier.format(`${name}`, options)
     });
     console.log(`${name} changed`);
-    // sync();
+    sync();
   });
 };
 
