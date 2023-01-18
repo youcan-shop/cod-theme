ed3b9280f6498552b9e6ff31a48e7d65dc04bebb
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2023-01-18 14:46:16 +0100

    update countdown script and logic

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,39 +1,63 @@
-function initCountdown() {
-  const currentDate = new Date();
-  let timeRemaining = targetDate - currentDate;
+const $ = elem => document.querySelector(elem);
 
-  const countdown = () => {
-    timeRemaining = targetDate - new Date();
+const countdown = function(_config) {
+  const tarDate = $(_config.target).getAttribute('data-date').split('-');
+  const day = parseInt(tarDate[0]);
+  const month = parseInt(tarDate[1]);
+  const year = parseInt(tarDate[2]);
+  let tarTime = $(_config.target).getAttribute('data-time');
+  let tarhour, tarmin;
 
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-    const progress = (timeRemaining / (targetDate - currentDate)) * 100;
-    const units = { days, hours, minutes, seconds };
+  if (tarTime != null) {
+    tarTime = tarTime.split(':');
+    tarhour = parseInt(tarTime[0]);
+    tarmin = parseInt(tarTime[1]);
+  }
 
-    for (const unit in units) {
-      const el = document.getElementById(unit);
-      if (units[unit] === 0) {
-        el.style.display = 'none';
-      } else if (el) {
-        const displayUnit = el.getAttribute('data-display-unit');
-        el.innerHTML = units[unit] + '<span class="unit">' + (displayUnit ? displayUnit : unit) + '</span>';
-      }
+  // Set the date we're counting down to
+  const countDownDate = new Date(year, month-1, day, tarhour, tarmin, 0, 0).getTime();
+
+  $(_config.target+' .day .word').innerHTML = _config.dayWord;
+  $(_config.target+' .hour .word').innerHTML = _config.hourWord;
+  $(_config.target+' .min .word').innerHTML = _config.minWord;
+  $(_config.target+' .sec .word').innerHTML = _config.secWord; 
+
+  const updateTime = () => {
+    // Get todays date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    requestAnimationFrame(updateTime);
+
+    $(_config.target+' .day .num').innerHTML = addZero(days);
+    $(_config.target+' .hour .num').innerHTML = addZero(hours);
+    $(_config.target+' .min .num').innerHTML = addZero(minutes);
+    $(_config.target+' .sec .num').innerHTML = addZero(seconds);
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      $(".countdown").innerHTML = "EXPIRED";
     }
+  }
 
-    const progressEl = document.getElementById('progress');
-    if (progressEl) {
-      progressEl.style.width = progress + '%';
-    }
-
-    if (timeRemaining < 0) {
-      document.querySelector('.countdown-wrapper').innerHTML = 'Offer Is Expired';
-    }
-  };
-
-  countdown();
-  setInterval(countdown, 1000);
+  updateTime();
 }
 
-initCountdown();
+const addZero = (x) => (x < 10 && x >= 0) ? "0"+x : x;
+
+
+countdown({
+  target: '.countdown',
+  dayWord: ' day',
+  hourWord: ' hour',
+  minWord: ' min',
+  secWord: ' sec'
+});
