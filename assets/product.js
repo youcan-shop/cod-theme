a682178e8daf86b00c8896da25fd70f4859fe905
Anas <93322743+bj-anas@users.noreply.github.com>
2023-03-03 11:51:46 +0100

    seperate quantity input logic

--- assets/product.js
+++ assets/product.js
@@ -420,28 +420,6 @@ function getSelectedVariants() {
   });
 }
 
-// Increment or Decrement custom quantity input
-
-function manipulateQuantity() {
-  const decrementButton = $('.decrement-button');
-  const incrementButton = $('.increment-button');
-  const quantityInput = $('.quantity-input');
-
-  decrementButton?.addEventListener('click', () => {
-    const currentValue = parseInt(quantityInput.value);
-    if (currentValue > 1) {
-      quantityInput.value = currentValue - 1;
-    }
-  });
-
-  incrementButton?.addEventListener('click', () => {
-    const currentValue = parseInt(quantityInput.value);
-    quantityInput.value = currentValue + 1;
-  });
-}
-
-manipulateQuantity();
-
 // Show selected quantity in checkout_step_2
 
 function getSelectedQuantity() {
