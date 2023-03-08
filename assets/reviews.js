73cdcbb99b0f477d6ac1b6407c0e5905eb8b7f8f
Anas <93322743+bj-anas@users.noreply.github.com>
2023-03-08 15:07:05 +0100

    self review

--- assets/reviews.js
+++ assets/reviews.js
@@ -27,8 +27,9 @@
 
     if (pagination.totalPages > 1) {
       showMoreButton.style.display = 'block';
-      showMoreButton.addEventListener('click', async () => {
+      showMoreButton?.addEventListener('click', async () => {
         const response = res.next();
+
         reviews = await response.data();
         reviewsWrapper.append(...reviews.map(review => createReviewItem(review)));
 
