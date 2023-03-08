d8d5970db1836d0885ea54da97ec2821be6bb5f2
Anas <93322743+bj-anas@users.noreply.github.com>
2023-03-08 15:32:24 +0100

    YSHOP2-130 : COD theme > Product page > UI enhancements (#220)
    ## JIRA Ticket
    
    [YSHOP2-130](https://youcanshop.atlassian.net/browse/YSHOP2-130)
    
    ## QA Steps
    
    -  [ ] `pnpm i`
    -  [ ] `pnpm dev`
    
    ## Note
    
    Leave empty when you have nothing to say
    
    
    [YSHOP2-130]:
    https://youcanshop.atlassian.net/browse/YSHOP2-130?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- assets/reviews-count.js
+++ assets/reviews-count.js
@@ -1,25 +1,24 @@
 (async () => {
-  const reviewsContainer = document.querySelector('.yc-general-review');
-  const reviewsWrapper = document.querySelector('.yc-general-review-wrapper');
+  const generalReviewsContainers = document.querySelectorAll('.yc-general-review');
+  const generalReviewsWrappers = document.querySelectorAll('.yc-general-review-wrapper');
+
   const noDataSetter = (element) => {
-    if (reviewsContainer) {
-      reviewsContainer.remove();
-    }
+    generalReviewsContainers.forEach(container => container.remove());
   };
 
   try {
-    const reviews = await youcanjs.product.fetchReviews(productId).data();
-    const reviewCount = reviews.length;
+    const totalReviews = await youcanjs.product.fetchReviews(productId).data();
+    const reviewCount = totalReviews.length;
 
-    reviewsContainer.style.display = 'block';
-    reviewsWrapper.innerHTML = `
+    generalReviewsContainers.forEach(container => container.style.display = 'block');
+    generalReviewsWrappers.forEach(wrapper => wrapper.innerHTML = `
       <li class='rating-stars'>
-        <div class="yc-reviews-stars" style="--rating: ${calculateAverageRating(reviews)};" aria-label="Rating of this product is ${calculateAverageRating(reviews)} out of 5"></div>
+        <div class="yc-reviews-stars" style="--rating: ${calculateAverageRating(totalReviews)};" aria-label="Rating of this product is ${calculateAverageRating(totalReviews)} out of 5"></div>
       </li>
       <li class='general-count'>
         (${reviewCount} ${ratings})
       </li>
-    `;
+    `);
 
     if (reviewCount === 0) {
       noDataSetter();
@@ -29,14 +28,14 @@
   }
 })();
 
-function calculateAverageRating(reviews) {
-  if (!reviews || !reviews.length) {
+function calculateAverageRating(totalReviews) {
+  if (!totalReviews || !totalReviews.length) {
     return 0;
   }
 
-  const totalRating = reviews.reduce((sum, review) => {
+  const totalRating = totalReviews.reduce((sum, review) => {
     return sum + review.ratings;
   }, 0);
 
-  return totalRating / reviews.length;
+  return totalRating / totalReviews.length;
 }
