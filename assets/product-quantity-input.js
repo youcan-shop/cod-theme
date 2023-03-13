436f96732ff207b2c0a24ff0b01e9cc44ab1a574
jamyoucn00 <mjamoun@youcan.shop>
2023-03-13 14:07:08 +0100

    add js docs

--- assets/product-quantity-input.js
+++ assets/product-quantity-input.js
@@ -1,10 +1,14 @@
-// Increment or Decrement custom quantity input
-
+/**
+ * Increment or Decrement custom quantity input
+ */
 function manipulateQuantity() {
   const decrementButton = $('.decrement-button');
   const incrementButton = $('.increment-button');
   const quantityInput = $('.quantity-input');
 
+  /**
+   * Decreases quantity value by 1 when decrement button is clicked
+   */
   decrementButton?.addEventListener('click', () => {
     const currentValue = parseInt(quantityInput.value);
     if (currentValue > 1) {
@@ -12,6 +16,9 @@ function manipulateQuantity() {
     }
   });
 
+  /**
+   * Increases quantity value by 1 when increment button is clicked
+   */
   incrementButton?.addEventListener('click', () => {
     const currentValue = parseInt(quantityInput.value);
     quantityInput.value = currentValue + 1;
