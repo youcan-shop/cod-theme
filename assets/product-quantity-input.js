bc3c4f3bc83cd8daa349d3c0ab5f0c4d78a32bb6
Youssef <117163420+void2023@users.noreply.github.com>
2023-03-17 11:59:15 +0100

    YSHOP2-163: As a seller, I want to be able to add the quantity  (#11)
    * style: style quantity button
    
    * fix: bug when adding multiple quantity fields in the same page
    
    ---------
    
    Co-authored-by: ybenadda <117163420+ybenadda@users.noreply.github.com>

--- assets/product-quantity-input.js
+++ assets/product-quantity-input.js
@@ -2,27 +2,25 @@
  * Increment or Decrement custom quantity input
  */
 function manipulateQuantity() {
-  const decrementButton = $('.decrement-button');
-  const incrementButton = $('.increment-button');
-  const quantityInput = $('.quantity-input');
+  const quantityFields = document.querySelectorAll('.quantity-field')
 
-  /**
-   * Decreases quantity value by 1 when decrement button is clicked
-   */
-  decrementButton?.addEventListener('click', () => {
-    const currentValue = parseInt(quantityInput.value);
-    if (currentValue > 1) {
-      quantityInput.value = currentValue - 1;
-    }
-  });
+  quantityFields.forEach(field => {
+    const decrementButton = field.querySelector('.decrement-button');
+    const incrementButton = field.querySelector('.increment-button');
+    const quantityInput = field.querySelector('.quantity-input');
 
-  /**
-   * Increases quantity value by 1 when increment button is clicked
-   */
-  incrementButton?.addEventListener('click', () => {
-    const currentValue = parseInt(quantityInput.value);
-    quantityInput.value = currentValue + 1;
-  });
+    decrementButton?.addEventListener('click', () => {
+      const currentValue = parseInt(quantityInput.value);
+      if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+      }
+    });
+
+    incrementButton?.addEventListener('click', () => {
+      const currentValue = parseInt(quantityInput.value);
+      quantityInput.value = currentValue + 1;
+    });
+  })
 }
 
 manipulateQuantity();
