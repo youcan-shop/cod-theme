c4d861ef7c66d2733543db0bec47493d44f53e1d
adildev101 <108662497+adildev101@users.noreply.github.com>
2023-04-27 14:37:27 +0000

    YSHOP2-416: For loading optimization load the video thumbnail first before you load the video (#335)
    ## JIRA Ticket
    
    [YSHOP2-416](https://youcanshop.atlassian.net/browse/YSHOP2-416)
    
    ## QA Steps
    
    - [ ] `pnpm i`
    - [ ] `pnpm dev`
    
    ## Note
    
    Leave empty when you have nothing to say
    
    
    [YSHOP2-416]:
    https://youcanshop.atlassian.net/browse/YSHOP2-416?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- assets/video.js
+++ assets/video.js
@@ -1,30 +1,74 @@
-function generateVideoSrc(videoLink, videoType) {
-  let videoId = '';
-  let videoSrc = '';
+function getBestThumbnailUrl(videoId) {
+  return new Promise((resolve, reject) => {
+    try {
+      const img = new Image();
+      const maxResUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
 
-  if (videoLink.includes('youtu.be')) {
-    videoId = videoLink.split('youtu.be/')[1];
-  } else {
-    videoId = videoLink.split('v=')[1];
-  }
+      img.src = maxResUrl;
 
-  if (videoType == 'youtube') {
-    videoSrc = 'https://www.youtube.com/embed/' + videoId;
-  } else if (videoType == 'facebook') {
-    videoSrc = `https://www.facebook.com/plugins/video.php?href=${videoLink}`;
-  }
+      img.onload = () => {
+        resolve(maxResUrl);
+      };
 
-  return videoSrc;
+      img.onerror = () => {
+        resolve(`https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`);
+      };
+    } catch (error) {
+      const defaultImageUrl = defaulVideotImage;
+      resolve(defaultImageUrl);
+    }
+  });
 }
 
-const videoSrc = generateVideoSrc(videoLink, videoType);
+function extractYouTubeVideoId(url) {
+  const regex = /(?:v=|youtu\.be\/)([^&]+)/;
+  const match = url.match(regex);
 
-if (videoType == 'youtube') {
-  const iframe = document.getElementById('youtube-video');
-  iframe.src = videoSrc;
-  iframe.width = width;
-} else if (videoType == 'facebook') {
-  const iframe = document.getElementById('facebook-video');
-  iframe.src = videoSrc;
-  iframe.width = width;
+  return match ? match[1] : null;
 }
+
+async function initializeYouTubeVideo(youtubeVideo) {
+  const spinner = document.querySelector('.spinner');
+
+  const videoLink = youtubeVideo.dataset.videoLink;
+  const videoId = extractYouTubeVideoId(videoLink);
+  youtubeVideo.style.width = width + 'px';
+  youtubeVideo.style.height = height + 'px';
+  const youtubeThumbnail = youtubeVideo.querySelector('.youtube-thumbnail');
+  const youtubePlayButton = youtubeVideo.querySelector('.youtube-play-button');
+
+  const thumbnailUrl = await getBestThumbnailUrl(videoId);
+  youtubeThumbnail.src = thumbnailUrl;
+
+  const iframe = document.createElement('iframe');
+  iframe.width = width;
+  iframe.height = height;
+  iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+  iframe.allowFullscreen = true;
+  iframe.style.display = 'none';
+  youtubeVideo.appendChild(iframe);
+
+  youtubeVideo.addEventListener('click', function () {
+    youtubeThumbnail.style.display = 'none';
+    youtubePlayButton.style.display = 'none';
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&playsinline=1`;
+    iframe.style.display = 'block';
+    spinner.style.display = 'block';
+
+    iframe.addEventListener('load', function () {
+      spinner.style.display = 'none';
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const youtubeVideo = document.getElementById('youtube-video');
+  const facebookVideo = document.getElementById('facebook-video');
+  
+  if (youtubeVideo) {
+    await initializeYouTubeVideo(youtubeVideo);
+  } else if (facebookVideo) {
+    facebookVideo.width = width;
+    facebookVideo.src = `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(videoLink)}`;
+  }
+});
