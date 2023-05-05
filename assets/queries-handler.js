2db6336ca3c961cede6b2d15d08679deea375a91
adildev101 <108662497+adildev101@users.noreply.github.com>
2023-05-05 11:25:29 +0100

    add the pagination working (#95)

--- assets/queries-handler.js
+++ assets/queries-handler.js
@@ -1,7 +1,47 @@
 const searchTitle = document.querySelector('#search-title');
 const urlParams = new URLSearchParams(window.location.search);
+const paginateBtnPrev = document.querySelector('.paginate-btn-prev');
+const paginateBtnNext = document.querySelector('.paginate-btn-next');
+const paginateBtnCurrent = document.querySelectorAll('.paginate-btn');
+let page = +urlParams.get('page[cod]');
+
 const query = urlParams.get('q');
 
 if (searchTitle) {
   searchTitle.innerHTML = query;
+}
+
+/**
+ * pagination
+ */
+const updateUrl = (key, value, url) => {
+  if (url.searchParams.has(key)) {
+    url.searchParams.set(key, value);
+  } else {
+    url.searchParams.append(key, value);
+  }
+};
+
+const convertUrl = (key, value) => {
+  const url = new URL(window.location.href);
+
+  updateUrl(key, value, url);
+
+  return url;
+};
+
+if (page <= 1 || !page) {
+  page = 1;
+}
+
+if (paginateBtnPrev) {
+  paginateBtnPrev.setAttribute('href', convertUrl('page[cod]', page - 1));
+}
+
+if (paginateBtnNext) {
+  paginateBtnNext.setAttribute('href', convertUrl('page[cod]', page + 1));
+}
+
+if (paginateBtnCurrent) {
+  paginateBtnCurrent.forEach((btn) => btn.setAttribute('href', convertUrl('page[cod]', btn.dataset.index)));
 }
