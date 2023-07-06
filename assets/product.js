36b2d43d6dd2eb245d61065f0d76ca0f28e94c9a
Anas <93322743+bj-anas@users.noreply.github.com>
2023-07-06 16:37:50 +0100

    DARK : fix product price in COD theme V2  (#118)

--- assets/product.js
+++ assets/product.js
@@ -232,11 +232,15 @@ function updateProductDetails(parentSection, image, price, variations) {
     const showStickyCheckoutPrice = $('#sticky-price');
 
     productPrices.forEach(productPrice => {
-      productPrice.innerHTML = `${
-        String(productPrice.innerHTML).split(' ')[0]
-      } ${price}`;
+      const priceText = productPrice.innerText;
+      const currencySymbol = priceText.replace(/[0-9.,]/g, "").trim();
+      const displayValue = `${price} ${currencySymbol}`;
 
-      showStickyCheckoutPrice.innerHTML = productPrice.innerHTML;
+      productPrice.innerText = displayValue;
+
+      if(showStickyCheckoutPrice) {
+        showStickyCheckoutPrice.innerHTML = productPrice.innerHTML;
+      }
     })
   }
 
