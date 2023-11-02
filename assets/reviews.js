9796c5cd22a23362a4aea2e2487a530171efc535
adildev101 <108662497+adildev101@users.noreply.github.com>
2023-11-02 09:36:24 +0100

    YSHOP2-807: Theme 1, 2 & 3 - Customers can add a review (#156)
    ## JIRA Ticket
    
    [YSHOP2-807](https://youcanshop.atlassian.net/browse/YSHOP2-807)
    
    ## QA Steps
    
    - [ ] `pnpm i`
    - [ ] `pnpm dev`
    
    ## Note
    
    Leave empty when you have nothing to say
    
    
    [YSHOP2-807]:
    https://youcanshop.atlassian.net/browse/YSHOP2-807?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- assets/reviews.js
+++ assets/reviews.js
@@ -125,6 +125,255 @@ const setupReviews = async () => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+const reviewData = {
+  content: '',
+  email: '',
+  ratings: 0,
+  first_name: '',
+  last_name: '',
+  images: []
+};
+
+const uploadedImagesHistory = [];
+
+function setupEventListeners() {
+  const reviewForm = document.getElementById('reviewForm');
+  const modal = document.getElementById("reviewModal");
+  const btn = document.getElementById("addReviewBtn");
+  const span = document.querySelector(".close");
+  const starRadios = document.querySelectorAll('.star-rating input[type="radio"]');
+
+  reviewForm.addEventListener('submit', handleReviewFormSubmit);
+  btn.addEventListener('click', showModal);
+  span.addEventListener('click', hideModal);
+
+  starRadios.forEach(radio => {
+    radio.addEventListener('change', function() {
+      const starCountSpan = document.querySelector('.review-stars-count');
+      starCountSpan.textContent = `(${this.value} ${ratings})`;
+    });
+  });
+
   setupReviews();
-});
+}
+
+async function handleReviewFormSubmit(e) {
+  e.preventDefault();
+  clearFieldErrors();
+
+  const formData = new FormData(e.target);
+  Object.assign(reviewData, {
+    content: sanitizeInput(formData.get('content')),
+    email: formData.get('email'),
+    ratings: Number(formData.get('ratings')),
+    first_name: sanitizeInput(formData.get('first_name')),
+    last_name: sanitizeInput(formData.get('last_name'))
+  });
+
+  try {
+      const response = await youcanjs.product.submitReview(reviewsProductId, reviewData);
+      if (response) {
+        notify(`${REVIEWS_TRANSLATED_TEXT.successMessage}`, 'success');
+        e.target.reset();
+        e.target.style.display = 'none';
+        document.querySelector('.thank-you-message').style.display = 'flex';
+        document.querySelector('.modal-title').style.display = 'none';
+      } else {
+        notify(`${REVIEWS_TRANSLATED_TEXT.errorMessage}`, 'error');
+      }
+  } catch (error) {
+      handleReviewError(error);
+  }
+}
+
+function handleReviewError(error) {
+  notify(error.message, 'error');
+  if (error) {
+      for (const field in error.meta.fields) {
+        const errorMsg = error.meta.fields[field][0];
+        displayFieldError(field, errorMsg);
+      }
+  } else {
+    notify(`${REVIEWS_TRANSLATED_TEXT.errorMessage}`, 'error');
+  }
+}
+
+function displayFieldError(fieldName, errorMsg) {
+  const inputElement = document.querySelector(`[name="${fieldName}"]`);
+  if (inputElement) {
+      const errorElement = document.createElement('span');
+      errorElement.className = 'field-error';
+      errorElement.textContent = errorMsg;
+      inputElement.parentElement.insertBefore(errorElement, inputElement.nextSibling);
+  }
+}
+
+function clearFieldErrors() {
+  const errorElements = document.querySelectorAll('.field-error');
+  errorElements.forEach(el => el.remove());
+}
+
+function showModal() {
+  const modal = document.getElementById("reviewModal");
+  modal.style.display = "flex";
+  document.body.style.overflow = 'hidden';
+}
+
+function hideModal() {
+  const modal = document.getElementById("reviewModal");
+  modal.style.display = "none";
+  document.body.style.overflow = '';
+}
+
+function sanitizeInput(input) {
+  const div = document.createElement('div');
+  div.innerHTML = input;
+  return div.textContent || div.innerText || "";
+}
+
+function uploadReviewImage(container, event) {
+  const targetElement = event.target;
+
+  const imgElement = container.querySelector('.uploaded-image');
+  if (imgElement && imgElement.style.display === 'block' && targetElement !== container.querySelector('.add-more')) {
+      return;
+  }
+
+  const uploadInput = createUploadInput();
+  uploadInput.addEventListener('change', handleFileChange);
+
+  function createUploadInput() {
+    const input = document.createElement('input');
+
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.style.display = 'none';
+    document.body.appendChild(input);
+    input.click();
+
+    return input;
+  }
+
+  function handleFileChange(event) {
+    const files = event.target.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = async () => {
+        try {
+          const res = await youcanjs.product.upload(file);
+          if (res && res.link) {
+            reviewData.images.push(res.link);
+            uploadedImagesHistory.push(res.link);
+
+            const imageUrl = reader.result;
+            displayUploadedImg(container, imageUrl);
+            appendImageToPreview(res.link, container.parentElement);
+            }
+          } catch (error) {
+            if (error.status === 422) {
+              const imageErrors = error.meta.fields.image;
+                if (imageErrors && imageErrors.length > 0) {
+                  notify(imageErrors[0], 'error');
+                } else {
+                    notify(error.detail, 'error');
+                  }
+                } else {
+                  notify(`${REVIEWS_TRANSLATED_TEXT.errorMessage}`, 'error');
+              }
+            }
+          };
+        }
+      }
+  }
+
+function displayUploadedImg(container, imageUrl) {
+  const imgElement = container.querySelector('.uploaded-image');
+  imgElement.src = imageUrl;
+  imgElement.style.display = 'block';
+  container.querySelector('.add-more').style.display = 'block';
+  container.querySelector('.yc-upload').style.display = 'none';
+}
+
+function appendImageToPreview(imageUrl, parent) {
+  let container = parent.querySelector('.yc-image-preview-container');
+  const submitButton = parent.querySelector('.yc-btn');
+
+  if (!container) {
+    container = document.createElement('div');
+    container.className = 'yc-image-preview-container';
+    parent.insertBefore(container, submitButton);
+  }
+
+  const imagePreviewSection = document.createElement('div');
+  const mainImage = document.querySelector('.uploaded-image');
+
+  imagePreviewSection.className = 'yc-image-preview';
+
+  const previewImage = document.createElement('img');
+  previewImage.src = imageUrl;
+  previewImage.onclick = function() {
+
+  mainImage.src = imageUrl;
+  mainImage.onclick = () => showExpandedImageView(mainImage);
+  parent.querySelectorAll('.yc-image-preview img').forEach(img => img.classList.remove('selected-image'));
+  previewImage.classList.add('selected-image');
+};
+
+  const deleteButton = createDeleteButton(imageUrl, imagePreviewSection);
+  imagePreviewSection.appendChild(previewImage);
+  imagePreviewSection.appendChild(deleteButton);
+  container.appendChild(imagePreviewSection);
+}
+
+function createDeleteButton(imageUrl, parentElement) {
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.className = 'remove-button';
+  button.innerHTML = 'X';
+
+  button.addEventListener('click', function() {
+    parentElement.remove();
+
+    const index = reviewData.images.indexOf(imageUrl);
+    if (index > -1) {
+        reviewData.images.splice(index, 1);
+        uploadedImagesHistory.splice(index, 1);
+    }
+
+    if (uploadedImagesHistory.length > 0) {
+        const lastImage = uploadedImagesHistory[uploadedImagesHistory.length - 1];
+        const mainImage = document.querySelector('.uploaded-image');
+        mainImage.src = lastImage;
+    } else {
+        resetMainUploadContainerIfNoImages();
+    }
+  });
+    return button;
+}
+
+function resetMainUploadContainerIfNoImages() {
+  if (reviewData.images.length === 0) {
+      const mainImage = document.querySelector('.uploaded-image');
+      const container = mainImage.closest('.yc-upload-container');
+      mainImage.src = '';
+      mainImage.style.display = 'none';
+      container.querySelector('.add-more').style.display = 'none';
+      container.querySelector('.yc-upload').style.display = 'flex';
+  }
+}
+
+function showExpandedImageView(imgElement) {
+  const bigView = document.querySelector('.image-big-view');
+  const bigImg = bigView.querySelector('img');
+  bigImg.src = imgElement.src;
+  bigView.style.display = 'flex';
+}
+
+function hideExpandedImageView(bigViewElement) {
+  bigViewElement.style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', setupEventListeners);
