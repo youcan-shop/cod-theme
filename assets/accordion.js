10d93432bf2880ad78e77c27a7d1f1cb0ff54155
bj-anas <93322743+bj-anas@users.noreply.github.com>
2024-04-01 13:09:32 +0000

    fix accordion

--- assets/accordion.js
+++ assets/accordion.js
@@ -1,24 +1,27 @@
+function accordionClickHandler() {
+  this.classList.toggle("active");
+  const accordionContent = this.nextElementSibling;
+
+  if (accordionContent.style.maxHeight) {
+    /**
+    * If the accordion is currently open, then close it
+    */
+    accordionContent.style.maxHeight = null;
+  } else {
+    /**
+    * If the accordion is currently closed, then open it
+    */
+    accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+  }
+}
+
 function setupAccordion() {
   const accordionHeaders = document.querySelectorAll(".accordion-header");
 
   accordionHeaders.forEach((accordion) => {
-    accordion.addEventListener("click", function () {
-      this.classList.toggle("active");
-      
-      const accordionContent = this.nextElementSibling;
-      
-      if (accordionContent.style.maxHeight) {
-        /**
-         * If the accordion is currently open, then close it
-        */
-        accordionContent.style.maxHeight = null;
-      } else {
-        /**
-         * If the accordion is currently closed, then open it
-        */
-        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-      }
-    });
+    // remove the event listener before adding it to prevent duplicates
+    accordion.removeEventListener("click", accordionClickHandler);
+    accordion.addEventListener("click", accordionClickHandler);
   });
 }
 
